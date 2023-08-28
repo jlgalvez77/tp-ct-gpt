8687ebbbc00e0aa90f88434bbeee394473ca90b1
jlgalvez77 <jlgalvez77@gmail.com>
2023-08-28 18:00:53 +0200

    Snippet Code

--- 01 - Snippet Code/Parte 1/03-regex.js
+++ 01 - Snippet Code/Parte 1/03-regex.js
@@ -1,7 +1,7 @@
 // Este regex debe validar que un string sea un correo electrónico válido.
 
 function validarCorreo() {
-   let tuRegex; // Agrega tu regex
+   let tuRegex = /^[\w\.-]+@[\w\.-]+\.\w+$/; // Agrega tu regex
    return tuRegex;
 }
 
